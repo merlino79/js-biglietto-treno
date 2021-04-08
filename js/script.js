e582927396bb1950b2920eb73069acc297a83f05
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-08 20:38:51 +0200

    prova calcolo accorciando un passaggio

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@
  var prezzo_intero = km * 0.21;
 
 
- var minorenni = prezzo_intero * 20 / 100;
- var calMin = prezzo_intero - minorenni;
- console.log(calMin);
+ var minorenni = prezzo_intero * 0.8;
+ //  var calMin = prezzo_intero - minorenni;
+ //  console.log(calMin);
 
 
  var over = prezzo_intero * 40 / 100;
@@ -20,7 +20,7 @@
 
  if (age < 18) {
      document.getElementById('exit').innerHTML = "il prezzo con lo sconto del 20% è " +
-         calMin.toFixed(2) + " euro";
+         minorenni.toFixed(2) + " euro";
  } else if (age > 65) {
      document.getElementById('exit').innerHTML = "il prezzo scontato è " +
          cal_over.toFixed(2) + " euro";
